cd72ebe50d01cac217d69b49ee6f241cff342b6d
Gui Danin <guida@std.hac.ac.il>
2024-06-16 07:48:46 +0300

    Revert "Started Working on MainMenu,"
    This reverts commit 4e0b7f04d86395313cf02eaab3a6b67110f2f823.

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,14 @@
 #include "NBoard.h"
 #include "FenAlgorithm.h"
 
-#include "StateMachine.h"
-
 #include <iostream>
 
 int main()
 {
-	/*FenAlgorithm fen;
+	FenAlgorithm fen;
 	NBoard& inst = NBoard::instance();
 	fen.setBoard(inst.m_board, "RNBQKBNRPPPPPPPP8888pppppppprnbqkbnr");
 	inst.setBoard(inst.m_board);/// change
 	GameManager game;
-	game.update();*/
-
-	StateMachine game;
 	game.update();
 }
